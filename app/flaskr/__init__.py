607417febb36365e882bcc5bdcdbcb6b860958be
EvanY0 <113192702+EvanY0@users.noreply.github.com>
2023-01-11 11:01:50 -0500

    dfvgb

--- app/flaskr/__init__.py
+++ app/flaskr/__init__.py
@@ -25,7 +25,7 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/')
+    @app.route('/wrefijdvov')
     def hello():
         return 'Hello, World!'
 
