86ed4da696f999eecaea4ed12a4ae8ace14bad5d
Kentoc2 <113193436+Kentoc2@users.noreply.github.com>
2023-01-18 10:54:31 -0500

    yjtjjt

--- app/Scripts/activate.bat
+++ app/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Evan.Yegorov\Desktop\Github Repos\pokededx-project-EvanY0\api-projejct-evan-and-kento\app
+set VIRTUAL_ENV=C:\Users\kento.chalmers\Documents\GitHub\api-projejct-evan-and-kento\app
 
 if not defined PROMPT set PROMPT=$P$G
 
